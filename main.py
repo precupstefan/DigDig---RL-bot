b874db8602819e78f1846f6f325685fbb46ad3b9
Stefan P <psa9710@gmail.com>
2021-07-15 12:53:57 +0300

    Add continuous learning

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import pytesseract
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 
+from config import config
 from custom_gym import DigDigEnv
 from web_driver import browser, take_screenshot_and_text
 import gym
-
+import os.path
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 # try:
 env = DigDigEnv()
-# check_env(env,warn=True)
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000, log_interval=4)
-model.save("dqn-digdig")
+
+if not os.path.isfile(config["model"]):
+    model = PPO("MlpPolicy", env, verbose=1)
+else:
+    model=PPO.load(config["model"])
+
+while True:
+    model.learn(total_timesteps=10)
+    model.save(config["model"])
 
 browser.close()
 # except Exception as e:
